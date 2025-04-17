e193488ee1f28d7479564e228dace2ecb4f6d7c9
suhail-20 <suhailek209@gmail.com>
2025-04-17 11:15:42 +0530

    device preview packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:todyapp/featurs/home/home_page.dart';
 
 late double height;
 late double width;
 void main() {
-  runApp(MyApp());
+  runApp(DevicePreview(
+    enabled: true,
+      builder: (context) => MyApp()));
 }
 
 class MyApp extends StatelessWidget {
