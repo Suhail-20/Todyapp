0cdcd38afce02df26458f20f82e0c6e3ec363e4f
suhail-20 <suhailek209@gmail.com>
2025-04-21 11:05:09 +0530

    onboarding screens

---
+++ lib/featurs/auth/screens/onboarding_screens/onboarding_page3.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage3 extends StatefulWidget {
+  const OnboardingPage3({super.key});
+
+  @override
+  State<OnboardingPage3> createState() => _OnboardingPage3State();
+}
+
+class _OnboardingPage3State extends State<OnboardingPage3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/featurs/auth/screens/onboarding_screens/onbording_screens.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OnbordingScreens extends StatefulWidget {
+  const OnbordingScreens({super.key});
+
+  @override
+  State<OnbordingScreens> createState() => _OnbordingScreensState();
+}
+
+class _OnbordingScreensState extends State<OnbordingScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
