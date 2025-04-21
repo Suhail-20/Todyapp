c92d52b54fc0388756e209f4123de4cfe5a4b75a
jasmheed <jamsheedpmk@gmail.com>
2025-04-21 11:45:46 +0530

    google

--- lib/core/constans/constans.dart
+++ lib/core/constans/constans.dart
@@ -11,4 +11,5 @@ class AppColors {
   static const Color whiteColor = Color(0xffFFFFFF);
   static const Color blackColor = Color(0xff000000);
   static const Color dividerColor = Color(0xffE0E5ED);
+  static const Color googleColor = Color(0xffF3F5F9);
 }
