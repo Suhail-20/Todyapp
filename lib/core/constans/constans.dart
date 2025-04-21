193d808089ff94d6225cb949889de8e15dd5b11d
jasmheed <jamsheedpmk@gmail.com>
2025-04-21 11:38:51 +0530

    color

--- lib/core/constans/constans.dart
+++ lib/core/constans/constans.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static const logoPath = 'assets/images/logo.png';
-  static const onBoard3 = 'assets/images/Onboarding 3.png';
+  static const onBoard_3 = 'assets/images/Onboarding 3.png';
 }
 
 class AppColors {
   static const Color primaryColor = Color(0xff24A19C);
+  static const Color secondaryColor = Color(0xff767E8C);
   static const Color whiteColor = Color(0xffFFFFFF);
   static const Color blackColor = Color(0xff000000);
+  static const Color dividerColor = Color(0xffE0E5ED);
 }
