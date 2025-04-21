14375edd5388f52d1d120dcfef29508d5c3c7980
jasmheed <jamsheedpmk@gmail.com>
2025-04-21 10:32:44 +0530

    auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:todyapp/core/constans/constans.dart';
+import 'package:todyapp/featurs/auth/screens/sign_up_screen.dart';
 import 'package:todyapp/featurs/home/home_page.dart';
 
 var height;
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         debugShowCheckedModeBanner: false,
-        home: HomePage(),
+        home: SignUpScreen(),
       ),
     );
   }
