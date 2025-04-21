c04d266692f95a267576e12acf9c05c22901eaf2
suhail-20 <suhailek209@gmail.com>
2025-04-21 11:39:56 +0530

    onboarding chages

--- lib/featurs/auth/screens/onboarding_screens/onboarding_page1.dart
+++ lib/featurs/auth/screens/onboarding_screens/onboarding_page1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:todyapp/core/constans/constans.dart';
 
 class OnboardingPage1 extends StatefulWidget {
   const OnboardingPage1({super.key});
@@ -11,7 +13,14 @@ class _OnboardingPage1State extends State<OnboardingPage1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
 
+      ),
+      body: Column(
+        children: [
+          SvgPicture.asset(Constants.logoPath)
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:todyapp/featurs/auth/screens/register_screen.dart';
 import 'package:todyapp/featurs/auth/screens/sign_up_screen.dart';
 import 'package:todyapp/featurs/home/home_page.dart';
 
+import 'featurs/auth/screens/onboarding_screens/onboarding_page1.dart';
+
 var height;
 var width;
 void main() {
@@ -30,7 +32,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         debugShowCheckedModeBanner: false,
-        home: RegisterScreen(),
+        home: OnboardingPage1(),
       ),
     );
   }
