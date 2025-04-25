9509b25d76ae27f4b972e818d8e3d77d59333f74
jasmheed <jamsheedpmk@gmail.com>
2025-04-25 11:04:23 +0530

    hhh

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ void main() async {
 
   runApp(
     DevicePreview(
-      enabled: true, // set to false in production
+      enabled: false, // set to false in production
       builder: (context) => const ProviderScope(child: MyApp()),
     ),
   );
